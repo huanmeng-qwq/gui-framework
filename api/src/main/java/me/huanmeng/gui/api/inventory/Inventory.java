2271f97cc8aa9e93edcf8ef26cdc1cfc8a878172
huanmeng-qwq <1871735932@qq.com>
2025-01-17 15:55:48 +0800

    feat(inventory): Inventory添加设置title和获取title的方法

--- api/src/main/java/me/huanmeng/gui/api/inventory/Inventory.java
+++ api/src/main/java/me/huanmeng/gui/api/inventory/Inventory.java
@@ -1,6 +1,9 @@
 package me.huanmeng.gui.api.inventory;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.inventory.ItemStack;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * 2025/1/17<br>
@@ -9,9 +12,16 @@ import org.bukkit.inventory.ItemStack;
  * @author huanmeng_qwq
  */
 public interface Inventory {
+    @NonNull
     InventoryType type();
 
+    @Nullable
     ItemStack getItem(int slot);
 
-    void setItem(int slot, ItemStack item);
+    void setItem(int slot, @Nullable ItemStack item);
+
+    @NonNull
+    Component title();
+
+    void title(@NonNull Component title);
 }
