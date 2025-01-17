514f9f222dc7f3ceabc61096b21395eb406a1c72
huanmeng-qwq <1871735932@qq.com>
2025-01-17 12:28:49 +0800

    build: 删除无用的构建脚本内容

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,13 +5,5 @@ dependencies {
     compileOnlyApi("net.kyori:adventure-platform-bukkit:4.3.1")
     compileOnlyApi("net.kyori:adventure-text-serializer-legacy:4.14.0")
     compileOnlyApi("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
-    compileOnlyApi("com.github.retrooper:packetevents-spigot:2.7.0")
-}
-
-tasks.compileJava {
-    exclude("**/test/**")
-}
-
-tasks.processResources {
-    exclude("plugin.yml")
+    compileOnlyApi("com.github.retrooper:packetevents-api:2.7.0")
 }
